1d182ccf7006934775de35d18a985624e8dda92a
ezerssss <magbanuaezra@gmail.com>
2024-07-19 01:17:43 +0800

    make it run now lol

--- craco.config.js
+++ craco.config.js
@@ -1,21 +1,22 @@
 module.exports = {
     webpack: {
-      configure: (webpackConfig) => {
-        // Add the following lines to handle 'crypto' and 'fs' dependencies
-        webpackConfig.resolve.fallback = {
-          fs: require.resolve("browserify-fs"), // or 'empty' if you prefer an empty module
-          crypto: require.resolve('crypto-browserify'),
-          stream: require.resolve('stream-browserify'),
-        };
-  
-        // Add the 'module' configuration for handling .wasm files
-        webpackConfig.module.rules.push({
-          test: /\.wasm$/,
-          type: 'javascript/auto',
-        });
-  
-        return webpackConfig;
-      },
+        configure: (webpackConfig) => {
+            // Add the following lines to handle 'crypto' and 'fs' dependencies
+            webpackConfig.resolve.fallback = {
+                fs: require.resolve('browserify-fs'), // or 'empty' if you prefer an empty module
+                crypto: require.resolve('crypto-browserify'),
+                stream: require.resolve('stream-browserify'),
+                path: false,
+                vm: false,
+            };
+
+            // Add the 'module' configuration for handling .wasm files
+            webpackConfig.module.rules.push({
+                test: /\.wasm$/,
+                type: 'javascript/auto',
+            });
+
+            return webpackConfig;
+        },
     },
-  };
-  
+};
